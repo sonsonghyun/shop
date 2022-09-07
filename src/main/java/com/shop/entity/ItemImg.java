2ac01db118646c4b157aa1cf3a530d297a5732ad
ikhyeons <skantrkwl789@naver.com>
2022-09-07 15:30:30 +0900

    Initial commit

---
+++ src/main/java/com/shop/entity/ItemImg.java
@@ -0,0 +1,56 @@
+package com.shop.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity{
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    //이미지 파일명
+    private String imgName;
+
+    //원본 이미지 파일명
+    private String oriImgName;
+
+    //이미지 조회 경로
+    private String imgUrl;
+
+    // 썸네일 (대표이미지)
+    private String repimgYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
